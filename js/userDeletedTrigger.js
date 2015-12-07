3d269c410e479a0d7aa15a2fb4b76bb3adb11ebd
Steve Manweiler <swm@clearblade.com>
2015-12-07 14:26:04 -0600

    tweak

--- js/userDeletedTrigger.js
+++ js/userDeletedTrigger.js
@@ -1,8 +1,5 @@
 
 function userDeletedTrigger(req, resp) {
-    sysKey = req.systemKey;
-    var sysSec = req.systemSecret;
-    var userToken = req.userToken;
     publishTrigger(req, resp, "User", "UserDeleted");
     resp.success("Should have published by now");
 }
